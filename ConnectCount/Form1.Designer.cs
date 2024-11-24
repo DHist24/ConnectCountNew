d58f4afb424ca0adc41b01d6a41bd079246220b0
nataliya.kachmasheva <nataliya.kachmasheva@IPRW053>
2024-11-24 16:03:23 +0400

    Init

---
+++ ConnectCount/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ConnectCount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSer = new System.Windows.Forms.TextBox();
+            this.textBoxBase = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(37, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сервер:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(62, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "База:";
+            // 
+            // textBoxSer
+            // 
+            this.textBoxSer.Location = new System.Drawing.Point(127, 39);
+            this.textBoxSer.Name = "textBoxSer";
+            this.textBoxSer.Size = new System.Drawing.Size(180, 20);
+            this.textBoxSer.TabIndex = 2;
+            // 
+            // textBoxBase
+            // 
+            this.textBoxBase.Location = new System.Drawing.Point(126, 92);
+            this.textBoxBase.Name = "textBoxBase";
+            this.textBoxBase.Size = new System.Drawing.Size(180, 20);
+            this.textBoxBase.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(378, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(378, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(126, 146);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 43);
+            this.textBox1.TabIndex = 10;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(41, 214);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Connections";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(451, 300);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(378, 153);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Скрыть график";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(425, 530);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ver 22.03.24";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxBase);
+            this.Controls.Add(this.textBoxSer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Connections in DB.SQLClient";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSer;
+        private System.Windows.Forms.TextBox textBoxBase;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ConnectCount/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using System.Data.Common;
+
+//ver 07.03.24 Условие,если пустые поля
+//ver 11.03.24 Свойства формы: поверх всех окон
+//             Условие, если закрыли программу через крестик
+//             Построение графика
+//ver 15.03.24 Обновление графика при нажатии на старт
+//ver 22.03.24 По оси X  рисуется график а не сетка+ожидание 5 минут
+//ver 24.11.24 Запрет изменения размера формы,  запрет разворота на весь экран
+//             Нет доступа к базе данных
+
+namespace ConnectCount
+
+{
+    delegate void SendCountDel(string t);
+    
+
+    public partial class Form1 : Form
+    {
+        SendCountDel sCD;
+        Thread countingThread;
+        Boolean Pressed;
+        Boolean EmptyAddressInfo;
+        Boolean NoAccessDataBaseInfo;
+        int countForClear;
+        
+
+
+
+
+
+        public  Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            
+
+        }
+
+     
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            textBox1.ReadOnly = true;
+            chart1.ChartAreas[0].AxisX.Maximum = 300;
+            
+
+
+        }
+
+        private void EmptyAddress()
+        {
+
+            var servertext = textBoxSer.Text;
+            var databasename = textBoxBase.Text;
+
+            if (string.IsNullOrEmpty(servertext) || string.IsNullOrEmpty(databasename))
+            {
+                MessageBox.Show("Не удалось подключиться к базе данных. Заполните адрес подключения.", "Ошибка подключения", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+                EmptyAddressInfo = true;
+                return;
+                
+            }
+            else
+            {
+                EmptyAddressInfo = false;
+
+            }
+        }
+
+        
+
+        
+
+
+        private void RefreshDataGrid()
+        {
+            sCD = new SendCountDel(SendCounts);
+
+            var servertext = textBoxSer.Text;
+            var databasename = textBoxBase.Text;
+
+
+            while (true && Pressed == false)
+            {
+
+                SqlConnection sqlConn = new SqlConnection();
+                sqlConn.ConnectionString = (@"Data Source='" + servertext + "';Initial Catalog='" + databasename + "';User ID=operator;Password=operator");
+                SqlCommand sqlComm = new SqlCommand();
+
+                try
+                {
+                    sqlComm.Connection = sqlConn;
+                    sqlConn.Open();
+
+
+
+                    sqlComm.CommandText = "select count(dbid)  from sys.sysprocesses where dbid in (select dbid from  master.sys.sysdatabases where name = '" + databasename + "') group by dbid";
+
+                    object result = sqlComm.ExecuteScalar();
+                    if (result != null)
+
+                    {
+
+                        SendCounts(result.ToString());
+
+                    }
+                    sqlConn.Close();
+
+
+                    Thread.Sleep(1000);
+                    countForClear++;
+
+                }
+                catch
+                {
+
+
+                    MessageBox.Show("Не удалось подключиться к базе данных.", "Ошибка подключения", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+                    
+                   
+                    return;
+                }
+
+                
+                                
+            }
+           
+        }
+
+        private void CreateChart(string result)
+        {
+
+            if (countForClear == 300) //5 минут
+            {
+                chart1.Series["Connections"].Points.Clear();
+                countForClear = 0;
+            }
+            chart1.Series["Connections"].Points.AddY(result);
+
+        }
+
+
+        private void SendCounts(string result)//метод 2
+        {
+            if (textBox1.InvokeRequired)
+            {
+                Invoke(sCD, new object[] { result });
+
+            }
+            else
+            {
+                textBox1.Text = result;
+                if (checkBox1.Checked == false)
+                { CreateChart(result); }
+                
+
+            }
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EmptyAddress();
+            
+            if (EmptyAddressInfo ==false)
+            {
+                chart1.Series["Connections"].Points.Clear();
+                countForClear = 0;
+                Pressed = false;
+                button2.Enabled = true;
+                button1.Enabled = false;
+                checkBox1.Enabled = false;
+                textBoxSer.Enabled = false;
+                textBoxBase.Enabled = false;
+
+               
+               
+
+                countingThread = new Thread(new ThreadStart(RefreshDataGrid));
+                countingThread.Start();
+            }
+           
+
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button1.Enabled = true;
+            textBoxSer.Enabled = true;
+            textBoxBase.Enabled = true;
+            checkBox1.Enabled = true;
+
+            textBox1.Clear();
+            Pressed = true;
+
+        }
+
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (EmptyAddressInfo == true)
+            {
+                 countingThread.Abort();
+            }
+        }
+
+       
+    }
+}
